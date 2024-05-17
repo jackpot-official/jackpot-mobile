f83efeb708204edc7857a3d48ebdd0227d41a203
Riya Dev <riyasdev9@gmail.com>
2024-05-17 14:11:17 -0400

    app/index.js edits from video walkthrough

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,22 @@ import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
 
 import { COLORS, icons, images, SIZES } from '../constants';
+import { 
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome 
+} from '.../components';
 
 const Home = () => {
+
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                }}
+            />
+        </SafeAreaView>
     )
 }
 
