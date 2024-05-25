cadb6740dccbf76b4cfb808b5a63af34c9732e84
Riya Dev <riyasdev9@gmail.com>
2024-05-25 12:40:06 -0400

    Fixed log in sessions issue. Must fix home page null username bug.

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -3,7 +3,14 @@ import React, { useState } from 'react'
 import { icons } from '../constants'
 import { ResizeMode, Video } from 'expo-av';
 
-const VideoCard = ( { video: { title, thumbnail, video, creator: { username, avatar} } }) => {
+const VideoCard = ( {
+    video: {
+        title,
+        thumbnail,
+        video,
+        creator: { username, avatar},
+    },
+    }) => {
   const [play, setPlay] = useState(false);
   
     return (
