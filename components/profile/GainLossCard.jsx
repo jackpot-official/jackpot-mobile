9c31f840584cbd15b84223fad556951f46ba6fca
Riya Dev <riyasdev9@gmail.com>
2024-06-01 09:06:44 -0400

    GainLossCard and ProfileNavButton components in profile page created.

---
+++ components/profile/GainLossCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const GainLossCard = ({ gainloss, percentage }) => {
+  return (
+    <View className="bg-white w-2/3 m-5 rounded-xl shadow-md shadow-black-300 p-5 flex flex-col space-y-3 border border-gray-100">
+        <Text className="font-hmedium text-black text-2xl">
+            Today's G/L
+        </Text>
+
+        <Text className="font-hbold text-red-600 text-3xl self-center">
+            {gainloss}
+        </Text>
+
+        <Text className="font-hmedium text-red-400 text-lg">
+            {percentage}% in the last month
+        </Text>
+    </View>
+  );
+};
+
+export default GainLossCard;
---
+++ components/profile/ProfileNavButton.jsx
@@ -0,0 +1,17 @@
+import { TouchableOpacity, Text } from 'react-native'
+import React from 'react'
+
+const ProfileNavButton = ( {title, handlePress, containerStyles, textStyles, isLoading }) => {
+  return (
+    <TouchableOpacity
+        onPress={ handlePress }
+        activeOpacity={ 0.7 }
+        className={`bg-secondary-100 rounded-full justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+        disabled={ isLoading }
+    >
+        <Text className={ `text-primary font-hsemibold text-lg ${textStyles}` }>{ title }</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default ProfileNavButton
