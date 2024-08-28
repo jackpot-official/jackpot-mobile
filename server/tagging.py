d49301422c007487008cde4e538ee7dbec7a3d6c
Luke Zhu <lezhu221@gmail.com>
2024-08-27 22:08:17 -0400

    laptop bouta die

---
+++ server/tagging.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import torch
+import requests
+import numpy as np
+import pandas as pd
+from transformers import pipeline
+from transformers import AutoTokenizer,AutoModelForSequenceClassification
+
+def getBullishBearishTag(title, body):
+    allText = title + body
+
+    model_name = "ProsusAI/finbert"
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    pipe = pipeline("text-classification", model="ProsusAI/finbert")
+    
+    return
